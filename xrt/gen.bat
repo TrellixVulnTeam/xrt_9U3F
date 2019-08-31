2f95e351b9eae39758507654e025a1faac498a32
chenchunliang <chenchunliang@baidu.com>
2019-09-01 01:32:02 +0800

    fix

--- xrt/gen.bat
+++ xrt/gen.bat
@@ -1,4 +1,4 @@
 set DEPOT_TOOLS_WIN_TOOLCHAIN=0
 set GYP_MSVS_VERSION=2017 
 set GYP_MSVS_OVERRIDE_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional
-call gn --root=src --ide=vs args out/win/debug
+call gn --root=src --ide=vs args out/win/debug --args="is_clang=false is_component_build=false use_sysroot=false use_custom_libcxx=false symbol_level=0"
--- xrt/gen.sh
+++ xrt/gen.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-gn --root=src args out/linux/debug
+
+gn --root=src args out/linux/debug --args="is_clang=false is_component_build=false use_sysroot=false use_custom_libcxx=false symbol_level=0"
